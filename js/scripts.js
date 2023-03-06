0a6404d8d9d8824969a5351443007bf87a70da39
Justin Shaffer <justinbshaffer91@gmail.com>
2023-03-06 14:39:22 -0800

    refactor business logic to prototype a la code

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,57 @@
 // Business Logic
-class PizzaOrder {
-  constructor(size, crust, toppings, quantity) {
-    this.size = size;
-    this.crust = crust;
-    this.toppings = toppings;
-    this.quantity = quantity;
+function PizzaOrder(size, crust, toppings, quantity) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.quantity = quantity;
+}
+
+PizzaOrder.prototype.calculateCost = function() {
+  const sizePrices = {
+    small: 8,
+    medium: 10,
+    large: 12
+  };
+  const crustPrices = {
+    thin: 0,
+    thick: 2,
+    "gluten-free": 3
+  };
+  const toppingPrices = {
+    pepperoni: 1,
+    sausage: 1,
+    bacon: 1,
+    ham: 1,
+    pineapple: 0.5,
+    mushrooms: 0.5,
+    onions: 0.5,
+    olives: 0.5,
+    "extra-cheese": 1
+  };
+
+  let baseCost = sizePrices[this.size];
+  baseCost += crustPrices[this.crust];
+
+  let toppingCost = 0;
+  for (let i = 0; i < this.toppings.length; i++) {
+    toppingCost += toppingPrices[this.toppings[i]];
   }
 
-  calculateCost() {
-    const sizePrices = {
-      small: 8,
-      medium: 10,
-      large: 12
-    };
-    const crustPrices = {
-      thin: 0,
-      thick: 2,
-      "gluten-free": 3
-    };
-    const toppingPrices = {
-      pepperoni: 1,
-      sausage: 1,
-      bacon: 1,
-      ham: 1,
-      pineapple: 0.5,
-      mushrooms: 0.5,
-      onions: 0.5,
-      olives: 0.5,
-      "extra-cheese": 1
-    };
+  const totalCost = (baseCost + toppingCost) * parseInt(this.quantity);
 
-    let baseCost = sizePrices[this.size];
-    baseCost += crustPrices[this.crust];
+  return totalCost;
+};
 
-    let toppingCost = 0;
-    for (let i = 0; i < this.toppings.length; i++) {
-      toppingCost += toppingPrices[this.toppings[i]];
-    }
+PizzaOrder.prototype.generateReceipt = function() {
+  const size = this.size;
+  const crust = this.crust;
+  const toppings = this.toppings;
+  const quantity = this.quantity;
+  const total = this.calculateCost();
 
-    const totalCost = (baseCost + toppingCost) * parseInt(this.quantity);
+  const toppingsList = toppings.map(topping => `<li>${topping}</li>`).join('');
 
-    return totalCost;
-  }
-
-  generateReceipt() {
-    const size = this.size;
-    const crust = this.crust;
-    const toppings = this.toppings;
-    const quantity = this.quantity;
-    const total = this.calculateCost();
-
-    const toppingsList = toppings.map(topping => `<li>${topping}</li>`).join('');
-
-    const receipt = `
+  const receipt = `
       <h2>Order Receipt</h2>
       <p><strong>Size:</strong> ${size}</p>
       <p><strong>Crust:</strong> ${crust}</p>
@@ -62,9 +61,8 @@ class PizzaOrder {
       <p><strong>Total Cost:</strong> $${total.toFixed(2)}</p>
     `;
 
-    return receipt;
-  }
-}
+  return receipt;
+};
 
 // User Interface Logic
 window.onload = function() {
